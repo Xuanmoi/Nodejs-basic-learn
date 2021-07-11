faa9b85770c198260c791001855b906555e8c861
Xuanmoi <45597744+Xuanmoi@users.noreply.github.com>
2021-07-11 16:21:15 +0800

    2021-07-11 learning

--- Buffer/buffer.js
+++ Buffer/buffer.js
@@ -1,3 +1,7 @@
 const buf=Buffer.from('runoob','ascii');
 console.log(buf.toString('hex'));
 console.log(buf.toString('base64'));
+
+//创建
+const buf1=Buffer.alloc(10);
+const buf2=Buffer.alloc(10,1);
